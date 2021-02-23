f0c547643d56cb5ec8ad64ec653a56787f21e6e6
sgruginshiz <55928403+sgruginshiz@users.noreply.github.com>
2021-02-23 12:03:04 -0500

    Lil spring cleaning
    Cleaned up project files
    Installed proper packages
    Created new namespace called "Handz"

---
+++ Handz/Assets/Scripts/XR/Rig/rigSettings.cs
@@ -0,0 +1,20 @@
+﻿// ---- S H R U G G E R - 2 0 2 1 ---- \\
+// - Not completed.
+// - Version 1.
+// - Purpose: HandzRig Settings Package
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Handz.Player.Rig
+{
+
+    public class rigSettings : ScriptableObject
+    {
+
+        
+
+    }
+
+}
